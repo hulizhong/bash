6a86c0d2c80fff9d07c731818bf4b0c1adb0aaa3
rabinhu <hulizhong@yeah.net>
2017-06-24 11:36:28 +0800

    Add shell ret value 0-255

--- getReturnValue.sh
+++ getReturnValue.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 
 return_ret()
@@ -13,9 +13,22 @@ echo_ret()
 
 return_ret
 aa=$?
-bb=$(echo_ret)
+#bb=$(echo_ret)
+bb=${echo_ret;}
 echo_ret
 cc=$?
 echo $aa
 echo $bb
 echo $cc
+
+
+
+############## $?只能返回0-255之间的数字。
+returnValue()
+{
+    size=256  #### bash里面变量默认为全局
+    return $size
+}
+returnValue
+echo "returnValue", $?, $size
+
